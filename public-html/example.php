7953d086721c295b3a54db631e0e11450e727426
jpercy <jeicopercy@gmail.com>
2021-05-03 11:14:21 -0500

    jp

--- public-html/example.php
+++ public-html/example.php
@@ -1,10 +1,7 @@
 <?php
+
 $myvar = "varname";
 $x = $_GET['arg'];
 eval("$myvar = $x;");
-
-$índice    = $argv[0]; // ¡Cuidado, no hay validación en la entrada de datos!
-$consulta  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $índice;";
-$resultado = pg_query($conexión, $consulta);
 
 ?>
